4095eaf8feadc47f3ca75b3c14315a0fafd18281
Asmaa-Alotaibi <asmaa-otaibi@yahoo.com>
2020-10-18 19:43:56 +0300

    task 2 !

---
+++ story.js
@@ -0,0 +1,4 @@
+let name="Asmaa";
+let age= 25;
+let hobby="swimming" ;  
+console.log(`Mr./Mrs.${name} , in ${age} years, you will be AGE_PLUS_${age} years old. By then you'll have decades of ${hobby} practice!''`);
